4596bbb1aafa80469a4e5f72f9c71d6e08bd912c
roshan5977 <roshannayak5977@gmail.com>
2023-10-05 11:39:28 +0530

    harb

---
+++ android/app/src/main/kotlin/com/example/harbinger_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.harbinger_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Harbinger());
+}
+
+class Harbinger extends StatelessWidget {
+  const Harbinger({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<Widget> _screens = [
+    // Content for Home tab
+    Container(
+      color: const Color.fromARGB(255, 248, 247, 246),
+      alignment: Alignment.center,
+      child: const Text(
+        'Workspace',
+        style: TextStyle(fontSize: 40),
+      ),
+    ),
+    // Content for Feed tab
+    Container(
+      color: Colors.purple.shade100,
+      alignment: Alignment.center,
+      child: const Text(
+        'Project',
+        style: TextStyle(fontSize: 40),
+      ),
+    ),
+    // Content for Favorites tab
+    Container(
+      color: Colors.red.shade100,
+      alignment: Alignment.center,
+      child: const Text(
+        'Favorites',
+        style: TextStyle(fontSize: 40),
+      ),
+    ),
+    // Content for Settings tab
+    Container(
+      color: Colors.pink.shade300,
+      alignment: Alignment.center,
+      child: const Text(
+        'Settings',
+        style: TextStyle(fontSize: 40),
+      ),
+    )
+  ];
+
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 244, 151, 45),
+        title: const Center(
+          child: Text(
+            "Harbinger",
+          ),
+        ),
+      ),
+      bottomNavigationBar: MediaQuery.of(context).size.width < 640
+          ? BottomNavigationBar(
+              currentIndex: _selectedIndex,
+              unselectedItemColor: Colors.grey,
+              selectedItemColor: Colors.indigoAccent,
+              // called when one tab is selected
+              onTap: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              // bottom tab items
+              items: const [
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.home), label: 'Workspace'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.feed), label: 'Project'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.favorite), label: 'Favorites'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.settings), label: 'Settings')
+                ])
+          : null,
+      body: Row(
+        children: [
+          if (MediaQuery.of(context).size.width >= 640)
+            NavigationRail(
+              onDestinationSelected: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              selectedIndex: _selectedIndex,
+              destinations: const [
+                NavigationRailDestination(
+                    icon: Icon(Icons.home), label: Text('Workspace')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.feed), label: Text('Project')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.favorite), label: Text('Favorites')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.settings), label: Text('Settings')),
+              ],
+
+              labelType: NavigationRailLabelType.all,
+              selectedLabelTextStyle: const TextStyle(
+                color: Colors.teal,
+              ),
+
+              unselectedLabelTextStyle: const TextStyle(),
+              // Called when one tab is selected
+              leading: const Column(
+                children: [
+                  SizedBox(
+                    height: 8,
+                  ),
+                  CircleAvatar(
+                    radius: 20,
+                    child: Icon(Icons.person),
+                  ),
+                ],
+              ),
+            ),
+          Expanded(child: _screens[_selectedIndex])
+        ],
+      ),
+    );
+  }
+}
