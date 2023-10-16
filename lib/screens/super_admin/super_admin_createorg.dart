543181a2650168e0fee29c17c7c8a707dfd6b41c
Aishwarya <sanku.aishwarya@feuji.com>
2023-10-16 13:04:39 +0530

    db connection create org

--- lib/screens/super_admin/super_admin_createorg.dart
+++ lib/screens/super_admin/super_admin_createorg.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:harbinger_flutter/models/organaisation_model.dart';
+import 'package:harbinger_flutter/screens/super_admin/super_admin_add_users.dart';
 import 'package:harbinger_flutter/services/organisation_service.dart';
-// Import your API service here
- // Import your Organisation model here
 
 class CreateOrganisation extends StatefulWidget {
   @override
@@ -15,7 +14,6 @@ class _CreateOrganisationState extends State<CreateOrganisation> {
   final TextEditingController _orgCodeController = TextEditingController();
   final TextEditingController _orgDescController = TextEditingController();
   DateTime? _startDate;
-  
   DateTime? _endDate;
 
   void _createOrganisation() async {
@@ -23,18 +21,19 @@ class _CreateOrganisationState extends State<CreateOrganisation> {
       orgName: _orgNameController.text,
       orgCode: _orgCodeController.text,
       orgDesc: _orgDescController.text,
-      orgStartDate: _startDate!,
-      orgEndDate: _endDate!,
+      orgStartDate: DateTime.now(),
+      orgEndDate: DateTime.now(),
       createdOn: DateTime.now(),
       updatedOn: DateTime.now(),
     );
 
     try {
       final createdOrg = await apiService.createOrganisation(newOrg);
-      // Handle the created organization as needed
-      print("Created Organization: ${createdOrg.orgName}");
+      // Show a custom pop-up dialog for success
+      _showSuccessDialog(createdOrg.orgName);
     } catch (e) {
-      print("Failed to create organization: $e");
+      // Show a custom pop-up dialog for error
+      _showErrorDialog("Failed to create organization: $e");
     }
   }
 
@@ -68,55 +67,117 @@ class _CreateOrganisationState extends State<CreateOrganisation> {
     }
   }
 
+  // Custom pop-up dialog for success
+  void _showSuccessDialog(String orgName) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Success"),
+          content: Text("Organization created: $orgName"),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => AddUsers()),
+  );
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Custom pop-up dialog for error
+  void _showErrorDialog(String errorMessage) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Error"),
+          content: Text(errorMessage),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Create Organisation'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('Enter Organisation Details', style: TextStyle(fontSize: 20)),
-            TextField(
-              controller: _orgNameController,
-              decoration: InputDecoration(labelText: 'Organization Name'),
+      body: Row(
+        children: <Widget>[
+          Image.asset(
+            'images/org.png',
+            width: 500,
+            height: double.infinity,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Enter Organisation Details', style: TextStyle(fontSize: 20)),
+                  TextField(
+                    controller: _orgNameController,
+                    decoration: InputDecoration(labelText: 'Organization Name'),
+                  ),
+                  TextField(
+                    controller: _orgCodeController,
+                    decoration: InputDecoration(labelText: 'Organization Code'),
+                  ),
+                  TextField(
+                    controller: _orgDescController,
+                    decoration: InputDecoration(labelText: 'Organization Description'),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      InkWell(
+                        onTap: () => _selectStartDate(context),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.calendar_today),
+                            Text(_startDate != null ? _startDate.toString() : 'Select Start Date'),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      InkWell(
+                        onTap: () => _selectEndDate(context),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.calendar_today),
+                            Text(_endDate != null ? _endDate.toString() : 'Select End Date'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  ElevatedButton(
+                    onPressed: _createOrganisation,
+                    child: Text('Create Organisation'),
+                  ),
+                ],
+              ),
             ),
-            TextField(
-              controller: _orgCodeController,
-              decoration: InputDecoration(labelText: 'Organization Code'),
-            ),
-            TextField(
-              controller: _orgDescController,
-              decoration: InputDecoration(labelText: 'Organization Description'),
-            ),
-            ElevatedButton(
-              onPressed: () => _selectStartDate(context),
-              child: Text(_startDate != null
-                  ? 'Start Date: ${_startDate!.toLocal()}'
-                  : 'Select Start Date'),
-            ),
-            ElevatedButton(
-              onPressed: () => _selectEndDate(context),
-              child: Text(_endDate != null
-                  ? 'End Date: ${_endDate!.toLocal()}'
-                  : 'Select End Date'),
-            ),
-            ElevatedButton(
-              onPressed: _createOrganisation,
-              child: Text('Create Organisation'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: CreateOrganisation(),
-  ));
 }
