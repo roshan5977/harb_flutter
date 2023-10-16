fd1ca6363e28b45430db2f7b75f839230bb18756
Aishwarya <sanku.aishwarya@feuji.com>
2023-10-16 14:23:18 +0530

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:harbinger_flutter/screens/login_screen.dart';
-//import 'package:harbinger_flutter/screens/super_admin/super_admin_createorg.dart';
+import 'package:harbinger_flutter/screens/projectadmin_screen.dart';
+import 'package:harbinger_flutter/screens/super_admin/super_admin_createorg.dart';
 import 'package:harbinger_flutter/utils/interceptor.dart';
 import 'package:harbinger_flutter/utils/shared_pref.dart';
 
@@ -16,3 +17,8 @@ void main() async {
         .add(AuthorizationInterceptor(token)); // Add interceptor with token
   }
 }
+// void main() {
+//   runApp(MaterialApp(
+//     home:ProjectAdminScreen(),
+//   ));
+// }
